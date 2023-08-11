46977b4e125adbfc2b749918a69056c57a9817f7
Kyusuk Oh <kyusukoh@berkeley.edu>
2023-08-11 01:44:20 -0700

    fixed errors and completed pacman animation

--- pacman.js
+++ pacman.js
@@ -1,14 +1,21 @@
 const canvas = document.getElementById("pacman-canvas");
 const ctx = canvas.getContext("2d");
 
-const pacSize = 30;
+const pacSize = 60;
 let pacX = canvas.width - pacSize;
-let pacY = 0;
+let pacY = canvas.height / 2;
 let pacAngle = 0;
 let pacMouthOpen = true;
+let pacDirection = "right";
 
 function drawPacMan() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (pacDirection === "right") {
+        pacX += 2; // Move Pac-Man to the right
+    } else {
+        pacX -= 2; // Move Pac-Man to the left
+    }
 
     ctx.beginPath();
     ctx.arc(pacX, pacY, pacSize, 0.2 * Math.PI, 1.8 * Math.PI, pacMouthOpen);
@@ -20,10 +27,18 @@ function drawPacMan() {
     pacAngle += 0.1;
     pacMouthOpen = !pacMouthOpen;
 
+    if (pacX > canvas.width) {
+        pacX = -pacSize; // Reset Pac-Man position to the left
+        pacDirection = "right";
+    } else if (pacX < -pacSize) {
+        pacX = canvas.width; // Reset Pac-Man position to the right
+        pacDirection = "left";
+    }
+
     requestAnimationFrame(drawPacMan);
 }
 
-canvas.width = 200;
-canvas.height = 200;
+canvas.width = 800;
+canvas.height = 400;
 
 drawPacMan();
